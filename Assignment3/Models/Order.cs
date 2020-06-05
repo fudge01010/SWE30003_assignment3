5978bb7d82cb6c9bb3c631f50525238804b329ae
Jack Fuge <jack.fuge@gmail.com>
2020-06-05 16:58:06 +1000

    adding more base classes

---
+++ Assignment3/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment3.Models
+{
+    class Order
+    {
+        private DateTime timeOpened;
+        private List<IItem> items;
+
+        public Order()
+        {
+            //constructor
+            timeOpened = DateTime.Now;
+            items = new List<IItem>();
+        }
+    }
+}
---
+++ Assignment3/Models/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment3.Models
+{
+    class Ticket
+    {
+        private List<IItem> items;
+        private DateTime timeOpened;
+        private readonly Order parentOrder;
+
+        public Ticket(Order parent)
+        {
+            //constructor
+            parentOrder = parent;
+        }
+    }
+}
