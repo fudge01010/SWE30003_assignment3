15136a3cb410924a71441ef14bc7bc4e798024b4
Jack Fuge <jack.fuge@gmail.com>
2020-06-05 16:59:25 +1000

    adding first skeleton (:toilet:) of dbmanager class.

---
+++ Assignment3/Helpers/DBManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment3.Helpers
+{
+    static public class DBManager
+    {
+        private static SQLiteConnection sqlite_conn;
+
+
+        public static void CreateConnection()
+        {
+            sqlite_conn = new SQLiteConnection("Data Source=database.db;Version=3;New=True;Compress=True;");
+            try
+            {
+                sqlite_conn.Open();
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public static void CloseConnection()
+        {
+            sqlite_conn.Close();
+        }
+
+        static public void CreateTable()
+        {
+            
+        }
+
+    }
+}
