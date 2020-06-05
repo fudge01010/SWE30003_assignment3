29dd302d206a9eeaeabd827d85d38635fa2b24eb
Jack Fuge <jack.fuge@gmail.com>
2020-06-05 23:17:55 +1000

    this might be the single :fire: spiciest bit of C# I've written in a HOT minute :interrobang:

--- Assignment3/Models/Ticket.cs
+++ Assignment3/Models/Ticket.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
 namespace Assignment3.Models
@@ -15,17 +17,38 @@ namespace Assignment3.Models
             //constructor
             parentOrder = parent;
             items = itemsOnTicket;
+            timeOpened = DateTime.Now;
         }
 
         public Order Parent()
         {
             return parentOrder;
         }
+
+        public DateTime TimeOpened()
+        {
+            return timeOpened;
+        }
+
         public int TableNumber()
         {
             //returns tableNumber, based on parent order
             //parentOrder.TableNumber();
             return 0;
         }
+
+        public List<FoodItem> KitchenItems()
+        {
+            List<FoodItem> foodItems = (items.FindAll(x => x.GetType() == typeof(FoodItem))).Cast<FoodItem>().ToList();
+            return foodItems;
+        }
+
+        public List<DrinkItem> BarItems()
+        {
+            List<DrinkItem> drinkItems = (items.FindAll(x => x.GetType() == typeof(DrinkItem))).Cast<DrinkItem>().ToList();
+            return drinkItems;
+        }
+
+        
     }
 }
